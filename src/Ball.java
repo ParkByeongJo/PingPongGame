993a3e8c6dc7f06f3f4145ae4abb1932a200ba11
삼성 <@LAPTOP-QDA7BU4T>
2018-11-29 23:55:27 +0900

    탁구대를 나타내는GameBoard클래스를 KeyListener클래스를 통하여 구현(implements)하고 JPanel클래스를 상속하여 만듬
    라켓 클래스(Racket)와 탁구공 클래스(Ball)에서 필드 변수들을 설정해놓음

--- src/Ball.java
+++ src/Ball.java
@@ -1,4 +1,9 @@
 
 public class Ball {
+	private static final int RADIUS=20;
+	int x=0;
+	int y=0;
+	int x_speed=1;
+	int y_speed=1;
 
 }
--- src/GameBoard.java
+++ src/GameBoard.java
@@ -1,6 +1,28 @@
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import javax.swing.*;
 
-public class GameBoard {
 
+public class GameBoard extends JPanel implements KeyListener {
+
+	@Override
+	public void keyPressed(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
